1a1dee175635f58d6aa6649e1398f3b9762d9c93
asmb123 <asmit.barua.phe23@itbhu.ac.in>
2025-10-17 08:52:26 +0530

    init

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get('/', (req, res)=>{
+    res.send("Working")
+})
+
+app.listen(3000, () => {
+    console.log("Server running in port 3000");
+})
